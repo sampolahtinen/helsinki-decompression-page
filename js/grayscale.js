7ff612119245746176c86e339e5084d359add903
Sampo Lahtinen <sampo.lahtinen@icloud.com>
2016-09-19 16:48:55 +0300

    Ver 0.3
    minor fixes

--- js/grayscale.js
+++ js/grayscale.js
@@ -31,15 +31,31 @@ $(document).ready(collapseNavbar);
 $(document).ready(changeHoverColor);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+// $(function() {
+//     $('a.page-scroll').bind('click', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top -200
+//         }, 500, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// });
+
+// Another jQuery page scrolling script
+// $(function() {
+//   $('a[href*="#"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top - 170
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+// });
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
